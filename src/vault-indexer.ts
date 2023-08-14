309fb9f2e87c95ca100fedaa2d49d4f5df375b71
tess3rac7 <tess3rac7@protonmail.com>
2023-08-14 17:52:22 -0400

    Remove dead code from vault-indexer

--- src/vault-indexer.ts
+++ src/vault-indexer.ts
@@ -1,6 +1,6 @@
-import { Address, BigInt } from "@graphprotocol/graph-ts"
+import { Address } from "@graphprotocol/graph-ts"
 import { StrategyAdded } from "../generated/ReaperVaultERC4626/ReaperVaultERC4626"
-import { Vault, Strategy } from "../generated/schema"
+import { Vault } from "../generated/schema"
 import { ReaperVaultERC4626 } from "../generated/templates";
 
 const addStrategyAddresses: Address[] = [
@@ -10,7 +10,7 @@ const addStrategyAddresses: Address[] = [
   Address.fromString("0x6539519E69343535a2aF6583D9BAE3AD74c6A293"), // degenicus
   Address.fromString("0x1E71AEE6081f62053123140aacC7a06021D77348"), // bongo
   Address.fromString("0xe00691e65cd4400c84a174a4c56f20ba43dffd89"), // ethos deployer
-  Address.fromString("0x4c3490df15edfa178333445ce568ec6d99b5d71c"), // ?
+  Address.fromString("0x4c3490df15edfa178333445ce568ec6d99b5d71c"), // eidolon
   // ... add other addresses that may call addStrategy on a new or existing vault 
 ];
 
@@ -21,12 +21,8 @@ export function handleStrategyAdded(event: StrategyAdded): void {
     return;
   }
 
-  // If vault entity does not exist:
-  //  1. create new data source for vault
-  //  2. save vault entity
-  //  3. save new strategy entity
-  // Steps 2 and 3 may be unnecessary, if, as the docs say, a new data source will also
-  // process the calls and events for the block in which it was created.
+  // If vault entity does not exist, create new data source for vault. The new data
+  // source will also process the calls and events for the block in which it was created.
   //
   // If vault entity already exists, it means that the vault data source must also exist.
   // In that case, we don't do anything here since the vault data source would trigger its own
@@ -35,13 +31,5 @@ export function handleStrategyAdded(event: StrategyAdded): void {
   let vaultEntity = Vault.load(vaultAddress);
   if (vaultEntity == null) {
     ReaperVaultERC4626.create(event.address);
-
-    // vaultEntity = new Vault(vaultAddress);
-    // vaultEntity.nrOfStrategies = BigInt.fromI32(1);
-    // vaultEntity.save();
-
-    // const strategyEntity = new Strategy(event.params.strategy.toHexString());
-    // strategyEntity.vault = vaultAddress;
-    // strategyEntity.save();
   }
 }
